18b4440f076f7600a54d8f485ccb5fa873448eff
Lucas Medeiros <lucaspontoe@hotmail.com>
2023-01-11 10:36:20 -0300

    Implement blur event

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 const form = document.querySelector(".form")
 const formSubmit = document.querySelector ("#form__button")
 formSubmit.addEventListener("click", function(event){
-    console.log("OI")
     event.preventDefault();
     form.classList.add("touched");
 })
 
-
 const inputEls = document.querySelectorAll(".form__input")
 inputEls.forEach(function(inputEl) {
-     inputEl.addEventListener("input", function() {
+     inputEl.addEventListener("blur", function() {
          inputEl.classList.add("touched")
-         console.log("opa")
      });
 })
